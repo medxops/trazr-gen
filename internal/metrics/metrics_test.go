eabb5c5455b10f92f55527cd89669de505daab05
Eric St-Pierre <stperic@outlook.com>
2025-05-30 09:54:00 -0400

    Update metrics_test.go

--- internal/metrics/metrics_test.go
+++ internal/metrics/metrics_test.go
@@ -264,7 +264,7 @@ func TestSimulateSum_EdgeCases(t *testing.T) {
 		{"nil meter provider", NewConfig(), zap.NewNop(), nil},
 	}
 	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
+		t.Run(tt.name, func(_ *testing.T) {
 			ctx, cancel := context.WithTimeout(context.Background(), 100*time.Millisecond)
 			defer cancel()
 			ch := make(chan struct{})
